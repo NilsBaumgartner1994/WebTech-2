835b6d495f2709b20a57317a2d25878db807a118
Nils Baumgartner <nilsbaumgartner1994@gmail.com>
2018-12-18 23:28:01 +0100

    .DS_Store bug Tools remove

--- iconeditor.py
+++ iconeditor.py
@@ -25,8 +25,9 @@ class IconEditorApp(webserver.App):
         tool_list = os.listdir("data/tools")
         tools_html = "<ul>"
         for tool_title in tool_list:
-            with open("data/tools/" + tool_title, "r") as g:
-                tools_html += "<li class=tool-list-item><img src='%s' title='%s' id='tool-%s' border='0'></li>" % (g.read(), tool_title,tool_title)
+            if tool_title != '.DS_Store':
+                with open("data/tools/" + tool_title, "r") as g:
+                    tools_html += "<li class=tool-list-item><img src='%s' title='%s' id='tool-%s' border='0'></li>" % (g.read(), tool_title,tool_title)
         tools_html += "</ul>"
         """End Toolslist"""
 

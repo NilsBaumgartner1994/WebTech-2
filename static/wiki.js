22dd11eac030d6a3fd777abfad8ca43adaee0ae3
Nils Baumgartner <nilsbaumgartner1994@gmail.com>
2018-12-19 01:21:06 +0100

    removed console logs

--- static/wiki.js
+++ static/wiki.js
@@ -1,9 +1,7 @@
 function addListenersForIcons(){
     var icons = document.getElementsByClassName("wikiicons-items"); //hole items aus der definierten icon Liste
     for (var icon_item of icons) { //hier nicht in, da wir die werte wollen https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Statements/for...of
-
         const icon = icon_item.childNodes[0]; //in dem listen element, befindet sich nur ein Bild //hier ggf. mehr sicherheit durch check
-        console.log(icon.title);
         //hier ist const notwendig, da sonst das zuletzt genommene Object übergeben wird, wir wollen die "Referenz" uns merken
         icon.addEventListener("click", function() {
             setIconTextArea(icon.title);
@@ -13,7 +11,6 @@ function addListenersForIcons(){
 
 function setIconTextArea(text){
     var textArea = document.getElementsByClassName("wikiicon");
-    console.log(textArea[0]);
     textArea[0].value = text;
 }
 
